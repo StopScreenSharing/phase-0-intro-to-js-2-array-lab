2b4674ad4b5698c6e1e8f43c8e726f0d10cec709
Aida Dieudonne <aidaharing10@gmail.com>
2024-09-04 23:07:49 -0600

    It is very late

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name) {
+   return cats.push(name)
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift(name)
+}
+function destructivelyRemoveLastCat() {
+    return cats.splice(cats.length-1)
+}
+function destructivelyRemoveFirstCat() {
+    return cats.splice(0, 1)
+}
+function appendCat(name) {
+    const newArray = [...cats]
+    newArray.push(name)
+    return newArray
+    // const newArray = cats.slice()
+    // newArray.push(name)
+    // return newArray
+}
+function prependCat(name) {
+    const newArray = [...cats]
+    newArray.unshift(name)
+    return newArray
+}
+function removeLastCat() {
+    const newArray = [...cats]
+    newArray.splice(cats.length-1)
+    return newArray
+}
+function removeFirstCat() {
+    const newArray = [...cats]
+    newArray.splice(0,1)
+    return newArray
+}
